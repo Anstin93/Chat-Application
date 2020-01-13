7d41159e152df1d72db3f72c1ec29db1b81bcfc5
Anstin <anstinroy@gmail.com>
2020-01-13 12:15:59 +0530

    Commit

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = " https://thawing-hollows-10324.herokuapp.com/";
+  const ENDPOINT = "https://react-chat-applicationnn.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
